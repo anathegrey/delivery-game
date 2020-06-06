b47d06184973da5889088cd292822cda02e82039
anathegrey <anajorgecsoaresalmeida@gmail.com>
2020-06-07 00:42:34 +0100

    falta confidence

--- deliverygame.py
+++ deliverygame.py
@@ -6,8 +6,8 @@ from tabulate import tabulate
 
 n_sim = 10000
 
-n_days = 5
-
+n_days = 120
+    
 compensation = [0.0, 0.5, 1.0, 1.5, 1.8]
 acceptance = [0.01, 0.25, 0.5, 0.6, 0.75]
 
@@ -36,7 +36,8 @@ def delivery_picked():
     
 def PF_total_cost(total_deliveries):
     num_delivery = 0
-    for i in range(total_deliveries):
+    i = 1
+    for i in range(total_deliveries+1):
         if i <= 10:
             num_delivery = num_delivery + i * 1
         elif i > 10:
@@ -48,98 +49,87 @@ def PF_total_cost(total_deliveries):
         
     
 def simulate():
-    remainder_lckr = 0
+    acc_deliveries_pf = 0
+    acc_deliveries_oc = 0
+    acc_deliveries_lckr = 0
+
+    daily_deliveries_pf = 0
+    daily_deliveries_oc = 0
+    daily_deliveries_lckr = 0
+
+    left_home = 0
+    left_lckr = 0
+
     remainder_home = 0
-    remainder_oc = 0
-    remainder_pf = 0
 
-    pf_cost = 0
-    oc_cost = 0
-    acc_cost = 0
+    status_home = 0
+    status_lckr = 0
 
-    n_deliveries_oc = 0
-    n_deliveries_pf = 0
-    n_deliveries_lckr = 0
+    acc_total_cost = 0
 
     accept = 0
-
-    rows = []
-        
-    for day in range(n_days):
-        n_home = 0
-        n_lckr = 0
-        
-        pf_cost_aux = 0
-        oc_cost_aux = 0
-        
-        n_deliveries_pf_aux = 0
-        n_deliveries_lckr_aux = 0
-        n_deliveries_oc_aux = 0
-        
-        remainder_lckr_aux = 0
-        remainder_oc_aux = 0
-        remainder_pf_aux = 0
-
-        new_acceptance = 0
-        
-        acc_cost_aux = 0
-
-        status_home = 0
-
-        status_lckr = 0
-        
-        expected_deliveries = random.randint(10, 50)
-        for j in range(expected_deliveries):
-            if (delivery_place()) == 'lckr':
-                n_lckr = n_lckr + 1
-            else:
-                n_home = n_home + 1
-        remainder_lckr_aux = n_lckr
-        for k in range(n_lckr):
-            if(delivery_picked() == 'picked up'):
-                n_deliveries_lckr_aux = n_deliveries_lckr_aux + 1
-                remainder_lckr_aux = remainder_lckr_aux - 1
-                if remainder_home > 0:
-                    new_acceptance = acceptance[accept]
-                    #print("Acceptance: ", new_acceptance)
-                    if(id_recipient(new_acceptance)) == 'OC':
-                        n_deliveries_oc_aux = n_deliveries_oc_aux + 1
-                        remainder_home = remainder_home - 1
-        n_deliveries_pf_aux = remainder_home + remainder_lckr_aux
-        
-        remainder_oc_aux = remainder_oc + n_deliveries_oc_aux
-        remainder_pf_aux = remainder_pf + n_deliveries_pf_aux
-        remainder_lckr_aux = remainder_lckr + n_deliveries_lckr_aux
-        
-        oc_cost_aux = n_deliveries_oc_aux * compensation[accept]
-        #print("Compensation: ", compensation[accept])
-        pf_cost_aux = PF_total_cost(n_deliveries_pf_aux)
-        acc_cost_aux = acc_cost + oc_cost_aux + pf_cost_aux
-
-        status_home = n_home + remainder_home
-        status_lckr = n_lckr + remainder_lckr
-        
-        if status_home < 0:
-            status_home = 0
-        if status_lckr < 0:
-            status_lckr = 0
-        
-        rows.append([day+1, n_home, n_lckr, n_deliveries_pf, n_deliveries_oc, n_deliveries_lckr, remainder_pf, remainder_oc, remainder_lckr, pf_cost, oc_cost, acc_cost, status_home, status_lckr])
-        n_deliveries_oc = n_deliveries_oc_aux
-        n_deliveries_pf = n_deliveries_pf_aux
-        n_deliveries_lckr = n_deliveries_lckr_aux
-        remainder_oc = remainder_oc_aux
-        remainder_pf = remainder_pf_aux
-        remainder_lckr = remainder_lckr_aux
-        oc_cost = oc_cost_aux
-        pf_cost = pf_cost_aux
-        acc_cost = acc_cost_aux
-        
-    print(tabulate(rows, headers=['DAY', 'NEW_HOME', 'NEW_LCKR', 'DEL_PF', 'DEL_OC', 'DEL_LOCKER', 'ACC_PF', 'ACC_OC', 'ACC_LCKR', 'c_PF', 'C_OC', 'C_ACC', 'STATUS_H', 'STATUS_L']))
-    accept = accept + 1
-    if accept == 4:
+    if accept == 5:
         accept = 0
 
+    new_acceptance = acceptance[accept]
+
+    rows = []
+    
+    for day in range(n_days):
+        new_home = 0
+        new_lckr = 0
+
+        deliver_oc_aux = 0
+        deliver_lckr_aux = 0
+        deliver_next_day = 0
+
+        daily_cost_pf = 0
+        daily_cost_oc = 0
+
+        expected_deliveries = random.randint(10, 50)
+
+        for e in range(expected_deliveries):
+            if delivery_place() == 'home':
+                new_home = new_home + 1
+            else:
+                new_lckr = new_lckr + 1
+
+        status_home = status_home + new_home - remainder_home
+        status_lckr = status_lckr + new_lckr - daily_deliveries_lckr
+
+        remainder_home = new_home
+        left_home = new_home
+        lckr_for = new_lckr + left_lckr
+        left_lckr = left_lckr + new_lckr
+        
+        for h in range(new_home):
+            if id_recipient(new_acceptance) == 'OC':
+                deliver_oc_aux = deliver_oc_aux + 1
+                left_home = left_home - 1
+
+        for l in range(lckr_for):
+            if delivery_picked() == 'picked up':
+                deliver_lckr_aux = deliver_lckr_aux + 1
+                left_lckr = left_lckr - 1
+            
+        deliver_next_day = left_home 
+
+        acc_deliveries_pf = acc_deliveries_pf + daily_deliveries_pf
+        acc_deliveries_oc = acc_deliveries_oc + daily_deliveries_oc
+        acc_deliveries_lckr = acc_deliveries_lckr + daily_deliveries_lckr
+
+        daily_cost_oc = daily_deliveries_oc * compensation[accept]
+        daily_cost_pf = PF_total_cost(daily_deliveries_pf)
+        acc_total_cost = acc_total_cost + daily_cost_oc + daily_cost_pf
+        
+        rows.append([day+1, new_home, new_lckr, daily_deliveries_pf, daily_deliveries_oc, daily_deliveries_lckr, acc_deliveries_pf, acc_deliveries_oc, acc_deliveries_lckr, daily_cost_pf, daily_cost_oc, acc_total_cost, status_home, status_lckr])
+        daily_deliveries_pf = deliver_next_day
+        daily_deliveries_oc = deliver_oc_aux
+        daily_deliveries_lckr = deliver_lckr_aux
+        
+    accept = accept + 1
+    print(tabulate(rows, headers=['DAY', 'NEW_HOME', 'NEW_LCKR', 'DEL_PF', 'DEL_OC', 'DEL_LOCKER', 'ACC_PF', 'ACC_OC', 'ACC_LCKR', 'c_PF', 'C_OC', 'C_ACC', 'STATUS_H', 'STATUS_L']))
+    
 simulate()
 
 '''
